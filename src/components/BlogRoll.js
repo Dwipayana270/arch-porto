fb14e4841b2caadd24a6831a3734ab64ebdc0fe6
Dwipayana270 <imadedwipayana123@gmail.com>
2022-04-08 18:50:14 +0800

    checking grid

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
-import {captionStyle4} from './Image.module.css'
+import {captionStyle4, wrapper} from './Image.module.css'
 
 class BlogRollTemplate extends React.Component {
   render() {
@@ -10,51 +10,51 @@ class BlogRollTemplate extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline pt-3 is-centered">
+      <div className='columns is-multiline is-centered px-0 mx-0 pt-3'>
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-                <header className='is-centered has-text-centered'>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <div className={captionStyle4}>
-                        <li>
-                          <Link to={post.fields.slug}>
-                            <PreviewCompatibleImage
-                              imageInfo={{
-                                image: post.frontmatter.featuredimage,
-                                alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                width:
-                                  post.frontmatter.featuredimage.childImageSharp
-                                    .gatsbyImageData.width,
-                                height:
-                                  post.frontmatter.featuredimage.childImageSharp
-                                    .gatsbyImageData.height,
-                              }}
-                            />
-                          </Link>
-                          <p>{post.frontmatter.title}</p>
-                        </li>
-                      </div>
+            // <div >
+              <div className='column is-half-tablet is-thirds-widescreen' key={post.id} style={{padding: 0}}>
+                {post.frontmatter.featuredimage ? (
+                  <div className={captionStyle4}> 
+                    <li>
                     
-                    </div>
-                  ) : null}
-                  {/* <p className="post-meta">
+                      <Link to={post.fields.slug}>
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.frontmatter.featuredimage,
+                            alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                            width:
+                              post.frontmatter.featuredimage.childImageSharp
+                                .gatsbyImageData.width,
+                            height:
+                              post.frontmatter.featuredimage.childImageSharp
+                                .gatsbyImageData.height,
+                          }}
+                        />
+                      </Link>
+                      <p>{post.frontmatter.title}</p>
+                    </li>
+                  </div>  
+                ) : null}
+
+              </div>
+                
+                  /* <p className="post-meta">
                     <Link
                       className="title has-text-primary is-size-4"
                       to={post.fields.slug}
                     >
                       {post.frontmatter.title}
                     </Link>
-                  </p> */}
-                </header>
-              {/* <article
+                  </p> */
+              /* <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? 'is-featured' : ''
                 }`}
               >
-              </article> */}
-            </div>
+              </article> */
+            
           ))}
       </div>
     )
@@ -92,7 +92,7 @@ export default function BlogRoll() {
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
-                        width: 300
+                        height: 500
                         quality: 100
                         layout: CONSTRAINED
                       )
