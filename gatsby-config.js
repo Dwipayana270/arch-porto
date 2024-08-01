bc7d676de78d3bcbe043d0dbc692259c56a2284b
Dwipayana270 <73886170+Dwipayana270@users.noreply.github.com>
2024-08-01 13:50:55 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        "icon": "src/images/favicon.png"
+        "icon": "src/images/favicon.ico"
       }
     },
     "gatsby-plugin-react-helmet",
